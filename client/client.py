5e4b38ac847fd54dedfdf736c5504d2f3b69514c
feixiaoxing <feixiaoxing@163.com>
2015-10-18 08:15:49 +0800

    fulfill client.py file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- client/client.py
+++ client/client.py
@@ -1,2 +1,25 @@
 
-client
+import pygame
+from pygame.locals import *
+from sys import exit
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 800
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('airplane fight')
+
+background = pygame.image.load("back.png")
+
+while True:
+	screen.fill(0)
+	screen.blit(background, (0,0))
+
+	pygame.display.update()
+	for event in pugame.event.get()
+		if event.type == pygame.QUIT
+			pygame.quit()
+			exit()
+
+
